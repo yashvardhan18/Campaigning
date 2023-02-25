f3b25addaa6d4b54345a98929e623e55e8bb7921
yashvardhan <yashvardhan@antiersolutions.com>
2023-02-25 22:50:39 +0530

    contract completed

--- contracts/campaign.sol
+++ contracts/campaign.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: UNLICENSED
 
-pragma solidity 0.8.17;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
@@ -19,7 +19,8 @@ contract crowdFunding is Initializable, ReentrancyGuardUpgradeable {
         address currencyToken;
         bool completed;
     }
-
+    cmp private detail;
+    mapping(address => mapping(uint256 => uint256)) public donationDetails;
     mapping(address => cmp) public campaignDetails;
 
     event crowdFund(
@@ -81,22 +82,47 @@ contract crowdFunding is Initializable, ReentrancyGuardUpgradeable {
                 campaignDetails[creator].details[campaignId].targetTime,
             "TTR"
         ); //Target Time Reached
-
-        campaignDetails[creator]
-            .details[campaignId]
-            .currentFundingAmount += donationAmount;
         IERC20(campaignDetails[creator].details[campaignId].currencyToken)
             .transferFrom(
                 msg.sender,
                 campaignDetails[creator].details[campaignId].creator,
                 donationAmount
             );
-
+        campaignDetails[creator]
+            .details[campaignId]
+            .currentFundingAmount += donationAmount;
+        donationDetails[msg.sender][campaignId] = donationAmount;
         if (
             campaignDetails[creator].details[campaignId].currentFundingAmount >=
             campaignDetails[creator].details[campaignId].targetAmount
         ) campaignDetails[creator].details[campaignId].completed = true;
 
         emit donated(campaignId, donationAmount);
+    }
+
+    function returnDonation(uint256 campaignId) external {
+        require(donationDetails[msg.sender][campaignId] > 0, "Invalid donor");
+        require(
+            block.timestamp < detail.details[campaignId].targetTime,
+            "Campaign not completed"
+        );
+        require(!detail.details[campaignId].completed, "Campaign is completed");
+        uint256 donation = donationDetails[msg.sender][campaignId];
+        detail.details[campaignId].currentFundingAmount -= donation;
+        donationDetails[msg.sender][campaignId] = 0;
+        IERC20(detail.details[campaignId].currencyToken).transferFrom(
+            detail.details[campaignId].creator,
+            msg.sender,
+            donation
+        );
+    }
+
+    function viewCampaignDetails(uint256 campaignId)
+        public
+        view
+        returns (campaign memory info)
+    {
+        info = detail.details[campaignId];
+        return info;
     }
 }
